21bd3934834c1038f2f5e07bc1ab2472503a007b
peterpozo <peteranderson.pozo@gmail.com>
2022-11-14 18:40:33 +0100

    add img

---
+++ js/app.js
@@ -0,0 +1,62 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    actualElement: 0,
+    sliderInterval: false,
+    slides: [
+      {
+        image: "01.webp",
+        title: "Marvel's Spiderman Miles Morale",
+        text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+      },
+      {
+        image: "02.webp",
+        title: "Ratchet & Clank: Rift Apart",
+        text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+      },
+      {
+        image: "03.webp",
+        title: "Fortnite",
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+      },
+      {
+        image: "04.webp",
+        title: "Stray",
+        text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+      },
+      {
+        image: "05.webp",
+        title: "Marvel's Avengers",
+        text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+      },
+    ],
+  },
+  methods: {
+    nextElement() {
+      if (this.actualElement === 4) {
+        this.actualElement = 0;
+      } else {
+        this.actualElement += 1;
+      }
+    },
+    previousElement() {
+      if (this.actualElement === 0) {
+        this.actualElement = 4;
+      } else {
+        this.actualElement -= 1;
+      }
+    },
+    setActualElement(index) {
+      this.actualElement = index;
+    },
+    carouselStop() {
+      clearInterval(sliderInterval);
+    },
+    carouselStart() {
+      sliderInterval = setInterval(this.nextElement, 1000);
+    },
+  },
+  mounted() {
+    sliderInterval = setInterval(this.nextElement, 1000);
+  },
+});
